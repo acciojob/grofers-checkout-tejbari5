f8072c823c9270564e659d579b478e0325cf4404
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-09-20 15:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let table = document.querySelectorAll("table")[0];
+let totalRow = document.createElement("tr");
+let totalCell = document.createElement("td");
+totalCell.id="ans";
+totalRow.appendChild(totalCell);
+table.appendChild(totalRow);
+
 const getSum = () => {
-//Add your code here
-  
+let priceArr = document.querySelectorAll(".price");
+	let sum=0;
+	for (let i = 0; i < priceArr.length; i++) {
+		sum+= +priceArr[i].innerHTML;
+	}
+  totalCell.innerText=sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
